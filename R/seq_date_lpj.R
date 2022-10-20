b11aa700848b45164bcbf44019144d1fcc6bb229
ben <ben.meyer@tum.de>
2022-10-20 18:10:48 +0200

    - error check since res is no longer being input manually but being parsed directly from input file
    - allow monthly temporal resolution
    
    - cleaner control flow, less redundancy

--- R/seq_date_lpj.R
+++ R/seq_date_lpj.R
@@ -9,17 +9,22 @@
 
 
 seq_date_lpj <- function(time, res, start = "1901-01-01") {
-  date_vec <- seq.Date(from = as.Date(start),
-                       length.out = length(time), by = res)
-  if (res == "year") {
-    date_vec <- format.Date(date_vec, "%Y")
+
+  if (!(res %in% c("day", "month", "year"))) {
+    error <- glue::glue("Time unit is: {res}. Must be one of: day, month, year")
+    return(print(error))
   }
+
+  date_vec <- seq.Date(from = as.Date(start), length.out = length(time), by = res)
+
   if (res == "day") {
     no_leap <- date_vec[format(date_vec, "%m-%d") != "02-29"]
     num_mis_date <- length(date_vec) - length(no_leap)
     missing_dates <- seq.Date(from = dplyr::last(date_vec),
                               length.out = num_mis_date + 1, by = res)
     date_vec <- c(no_leap, missing_dates[-1])
+    return(date_vec)
   }
+
   return(date_vec)
 }
