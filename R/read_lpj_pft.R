41fbc42142f187104d00b500dfa5db0a220fb505
ben <ben.meyer@tum.de>
2023-09-01 13:55:26 +0200

    units are now automatically added to the output tables

--- R/read_lpj_pft.R
+++ R/read_lpj_pft.R
@@ -18,13 +18,13 @@ read_lpj_pft <- function(nc_path, var_name) {
 
   var_name_internal <- paste0("Pft-Out/",var_name)
 
-  .add_attributes <- function(x, var_name, pfts, lat, lon, dates) {
+  .add_attributes <- function(x, var_name, unit_val, pfts, lat, lon, dates) {
     x |>
       t() |>
       tibble::as_tibble(.name_repair = "minimal") |>
       purrr::set_names(pfts) |>
       dplyr::mutate(date = rep(dates, length(lat)), lat = rep(lat, each = length(dates)),
-                    lon = rep(lon, each = length(dates)), var = var_name)
+                    lon = rep(lon, each = length(dates)), var = var_name, unit = unit_val)
   }
 
   nc   <- ncdf4::nc_open(nc_path)
@@ -53,18 +53,22 @@ read_lpj_pft <- function(nc_path, var_name) {
   if (length(lat) != 1) {
     if (length(var_name) != 1) {
 
-      out_tibble <- lapply(var_name_internal, ncdf4::ncvar_get, nc = nc) |>
-        lapply(as.data.frame) |>
-        purrr::map2(var_name, .add_attributes, pfts, lat, lon, dates) |>
+      unit_val <- lapply(var_name_internal, ncdf4::ncatt_get, nc = nc) |>
+        lapply(`[[`, "unit")
+      raw_var <- lapply(var_name_internal, ncdf4::ncvar_get, nc = nc) |>
+        lapply(as.data.frame)
+      out_tibble <- purrr::pmap(list(raw_var, var_name, unit_val), .add_attributes,
+                                pfts, lat, lon, dates) |>
         dplyr::bind_rows()
 
       ncdf4::nc_close(nc)
       return(out_tibble)
     }
 
+    unit_val <- ncdf4::ncatt_get(nc, var_name_internal)[["unit"]]
     out_tibble <- ncdf4::ncvar_get(nc, var_name_internal) |>
       as.data.frame() |>
-      .add_attributes(var_name, pfts, lat, lon, dates)
+      .add_attributes(var_name, unit_val, pfts, lat, lon, dates)
 
     ncdf4::nc_close(nc)
     return(out_tibble)
@@ -72,17 +76,19 @@ read_lpj_pft <- function(nc_path, var_name) {
 
   if (length(var_name) != 1) {
 
-    out_tibble <- lapply(var_name_internal, ncdf4::ncvar_get, nc = nc) |>
-      purrr::map2(var_name, .add_attributes, pfts, lat, lon, dates) |>
+    unit_val <- lapply(var_name_internal, ncdf4::ncatt_get, nc = nc) |>
+      lapply(`[[`, "unit")
+    raw_var <- lapply(var_name_internal, ncdf4::ncvar_get, nc = nc)
+    out_tibble <- purrr::pmap(list(raw_var, var_name, unit_val), .add_attributes, pfts, lat, lon, dates) |>
       dplyr::bind_rows()
 
     ncdf4::nc_close(nc)
     return(out_tibble)
   }
 
-
+  unit_val <- ncdf4::ncatt_get(nc, var_name_internal)[["unit"]]
   out_tibble <- ncdf4::ncvar_get(nc, var_name_internal) |>
-    .add_attributes(var_name, pfts, lat, lon, dates)
+    .add_attributes(var_name, unit_val, pfts, lat, lon, dates)
 
   ncdf4::nc_close(nc)
   return(out_tibble)
